616e488b6a4e6e73a865dec47e09a61a0b203cfd
Tan <tanamanda13@gmail.com>
2022-04-26 14:51:17 +0200

    1.3 - interface vide

--- td2/interfaces/1_2_interfaces.go
+++ td2/interfaces/1_2_interfaces.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func main() {
+func main2() {
 	err := run()
 	if err != nil {
 		log.Fatal(err)
---
+++ td2/interfaces/1_3_interfaces.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	// "reflect"
+)
+
+func main() {
+	PrintIt(true)
+}
+
+func PrintIt(input interface{}) {
+	switch input.(type) {
+	case int:
+		fmt.Println("c'est un int")
+	case string:
+		fmt.Println("c'est un string")
+	default:
+		fmt.Println("c'est autre chose d'un string ou int")
+	}
+}
