946578d11a33b3eb99ce2d3aca797b70155e2df9
Tan <tanamanda13@gmail.com>
2022-04-26 11:24:24 +0200

    1.1_1.a - implement interface 1.a

--- td2/interfaces/1_1_interfaces.go
+++ td2/interfaces/1_1_interfaces.go
@@ -5,11 +5,20 @@ import "fmt"
 type IPAddr [4]byte
 
 func main() {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
-	}
+	// Use IPAddr interface
+	var ip = IPAddr{1, 2, 3, 4}
+	fmt.Println(ip.String())
+
+	// hosts := map[string]IPAddr{
+	// 	"loopback":  {127, 0, 0, 1},
+	// 	"googleDNS": {8, 8, 8, 8},
+	// }
+	// for name, ip := range hosts {
+	// 	fmt.Printf("%v: %v\n", name, ip)
+	// }
+}
+
+// Implement fmt.Stringer interface
+func (ipAd *IPAddr)String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ipAd[0], ipAd[1], ipAd[2], ipAd[3])
 }
