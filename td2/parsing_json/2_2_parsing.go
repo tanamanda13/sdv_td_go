e367ce8e533bb92ad4d46fc42937c087fa96130a
Tan <tanamanda13@gmail.com>
2022-04-27 13:38:09 +0200

    td2 fin correction

--- td2/parsing_json/2_2_parsing.go
+++ td2/parsing_json/2_2_parsing.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
+
+	// "log"
 	"net/http"
 )
 
@@ -26,8 +27,9 @@ func main() {
 	}
 	fmt.Println(usersMap)
 
+	// http.Handle("/", &User{}) // en tapant sur localhost, si l'objet a bien la méthode ServeHTTP on va chercher l'objet User
 	http.HandleFunc("/", ServeHTTP)
-	log.Fatal(http.ListenAndServe(":4000", nil))
+	log.Fatal(http.ListenAndServe(":4001", nil)) // Démarre le server et écoute
 }
 
 type User struct {
@@ -36,6 +38,36 @@ type User struct {
 	UserID string
 }
 
-func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world and people!\n")
+// func (s *User) ServeHTTP(w http.ResponseWriter, r *http.Request) { // On rattache notre serverHTTP à un User -> Dispose maintenant de cette méthode. Si l'objet (User) possède cette méthode ServeHTTP, alors il peut répondre. Mon objet User (receiver) devient un Handler.
+func ServeHTTP(w http.ResponseWriter, r *http.Request) { // r -> requet (dans barre recherche, question) et w -> response
+	// io.WriteString(w, "Hello, world and people!\n")
+	isUserID, userIDKey := searchId(r)
+	// fmt.Println(userIDKey)
+
+	sendResponse(w, isUserID, userIDKey)
+}
+
+func searchId(r *http.Request) (bool, string) {
+	id := r.FormValue("id")
+	isUserID := false
+	userIDKey := ""
+	for k := range usersMap {
+		if k == id {
+			isUserID = true
+			userIDKey = k
+			break
+		}
+	}
+	return isUserID, userIDKey
+}
+
+func sendResponse(w http.ResponseWriter, isUserID bool, userIDKey string) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if isUserID {
+		w.WriteHeader(http.StatusOK)
+		b1, _ := json.Marshal(usersMap[userIDKey])
+		w.Write(b1)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
